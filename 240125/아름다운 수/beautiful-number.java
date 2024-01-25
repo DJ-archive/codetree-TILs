c0f7049b2e2e9bb4e6fc8071c4769cfee923120b
Dajeong Yun <58822617+DJ-archive@users.noreply.github.com>
2024-01-25 10:42:59 +0900

    Title: 아름다운 수, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

--- 240125/아름다운 수/beautiful-number.java
+++ 240125/아름다운 수/beautiful-number.java
@@ -3,49 +3,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
     static int[] arr;
     static int ans, n;
 
-    // 1. 가능한 모든 수를 만들고 아름다운 수인지 검증하는 방식
+    // 2. 아름다운 수를 만들어나가기 (1~4까지이므로 충분히 가능)
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         arr = new int[n+1];
-        findBeautifulNums(0);
+        findBeautifulNums(new StringBuilder(), 0);
         System.out.println(ans);
     }
 
-    private static void findBeautifulNums(int cnt) {
+    private static void findBeautifulNums(StringBuilder sb, int cnt) {
         if (cnt == n) {
-            if (isBeautiful(arr)) {
-                ans++;
-            }
+            ans++;
             return;
         }
-        for (int i = 1; i <= 4; i++) {
-            arr[cnt] = i;
-            findBeautifulNums(cnt + 1);
+        if (sb.length()+1 <= n) {
+            sb.append(1);
+            findBeautifulNums(sb, cnt+1);
+            sb.delete(cnt, cnt+1);
+        }
+        if (sb.length()+2 <= n) {
+            sb.append(22);
+            findBeautifulNums(sb, cnt+2);
+            sb.delete(cnt, cnt+2);
+        }
+        if (sb.length()+3 <= n) {
+            sb.append(333);
+            findBeautifulNums(sb, cnt+3);
+            sb.delete(cnt, cnt+3);
+        }
+        if (sb.length()+4 <= n) {
+            sb.append(4444);
+            findBeautifulNums(sb, cnt+4);
+            sb.delete(cnt, cnt+4);
         }
     }
-
-    // 인덱스 주의
-    private static boolean isBeautiful(int[] arr) {
-        for (int i = 0; i < n; i += arr[i]) {
-            // 연속해야 하는 수를 고려했을 때 길이가 벗어나는 경우 false
-            if (arr[i] + i > n) {
-                return false;
-            }
-            // 연속해야하는 수까지 살펴보았을 때 연속하지 않았을 경우
-            for (int j = i; j < i + arr[i]; j++) {
-                if (arr[j] != arr[i]) {
-                    return false;
-                }
-            }
-        }
-        // false에 걸리지 않으면 true
-        return true;
-    }
-
 
 }
