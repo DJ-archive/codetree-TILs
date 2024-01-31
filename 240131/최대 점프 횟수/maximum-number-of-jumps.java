957624b8d50a71d27261f0a170e59c42cbadb4bf
Dajeong Yun <58822617+DJ-archive@users.noreply.github.com>
2024-01-31 09:48:25 +0900

    Title: 최대 점프 횟수, Time: 117ms, Memory: 9MB, Status: Passed - CodeTree

--- 240131/최대 점프 횟수/maximum-number-of-jumps.java
+++ 240131/최대 점프 횟수/maximum-number-of-jumps.java
@@ -18,15 +18,13 @@ public class Main {
 
         int[] dp = new int[n];
         // 첫번째 (0)자리에서부터 i번째 자리까지 최대 점프 가능한 횟수
+        Arrays.fill(dp, -1);
         dp[0] = 0;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (arr[i] + i >= j) {
-                    if (i != 0 && dp[i] <= 0) {
-                        continue;
-                    }
-                    dp[j] = Math.max(dp[i] + 1, dp[i]);
-                }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] + j < i || dp[j] == -1) continue; // dp[j]가 -1이라면 해당 위치에 이동이 불가했다는 뜻이므로 계산해서 제외해주는 방법
+                dp[i] = dp[j] + 1;
             }
         }
 
