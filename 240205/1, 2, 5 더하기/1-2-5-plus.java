db0c11536332dfafb800b7fe2e88e278caadc9c1
Dajeong Yun <58822617+DJ-archive@users.noreply.github.com>
2024-02-05 03:34:56 +0900

    Title: 1, 2, 5 더하기, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240205/1, 2, 5 더하기/1-2-5-plus.java
+++ 240205/1, 2, 5 더하기/1-2-5-plus.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    static final int MOD = 10007;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -17,6 +18,7 @@ public class Main {
                     dp[i] += dp[i-num[j]];
                 }
             }
+            dp[i] %= MOD;
         }
         System.out.println(dp[n]);
     }
