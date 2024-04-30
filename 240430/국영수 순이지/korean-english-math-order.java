8784e84559d1303580d2b29994adcd555b3015a4
Dajeong Yun <58822617+2eve2@users.noreply.github.com>
2024-04-30 15:35:55 +0900

    Title: 국영수 순이지, Time: 268ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240430/국영수 순이지/korean-english-math-order.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+class Person {
+    String n;
+    int l;
+    int e;
+    int m;
+
+    public Person(String n, int l, int e, int m) {
+        this.n = n;
+        this.l = l;
+        this.e = e;
+        this.m = m;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        Person[] arr = new Person[n];
+        for(int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = new Person(st.nextToken(), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+        }
+
+        Arrays.sort(arr, (a, b)-> {
+            if(b.l == a.l) {
+                if(b.e == a.e) {
+                    return b.m - a.m;
+                } else {
+                    return b.e - a.e;
+                }
+            } else {
+                return b.l - a.l;
+            }
+        });
+
+        for(Person p : arr) {
+            System.out.println(String.format("%s %d %d %d", p.n, p.l, p.e, p.m));
+        }
+    }
+}
